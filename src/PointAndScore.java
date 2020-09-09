8e36d126487abe31118cdaa43638967bdd98ce10
= <=>
2020-09-09 18:05:33 +0700

    commit to repository

---
+++ src/PointAndScore.java
@@ -0,0 +1,10 @@
+public class PointAndScore {
+
+    public int score;
+    public Point point;
+
+    public PointAndScore(int score, Point point){
+        this.score = score;
+        this.point = point;
+    }
+}
---
+++ src/TicTacToe.java
@@ -0,0 +1,55 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class TicTacToe {
+    public static final Random RANDOM = new Random();
+
+    public static void main(String[] args) {
+        Board b = new Board();
+        Scanner scanner = new Scanner(System.in);
+        b.displayBoard();
+        System.out.println("Select turn:\n1. Computer(X) / 2. User(O): ");
+
+        int choice = scanner.nextInt();
+
+        if(choice == Board.PLAYER_X){
+            Point p = new Point(RANDOM.nextInt(3), RANDOM.nextInt(3));
+            b.placeAMove(p, Board.PLAYER_X);
+            b.displayBoard();
+        }
+
+        while (!b.isGameOver()){
+            boolean moveOK = true;
+
+            do{
+                if(!moveOK){
+                    System.out.println("Cell already filled");
+                }
+
+                System.out.println("Your move: ");
+
+                Point userMove = new Point(scanner.nextInt(), scanner.nextInt());
+
+                moveOK = b.placeAMove(userMove, Board.PLAYER_O);
+            }
+            while (!moveOK);
+            b.displayBoard();
+
+            if(b.isGameOver()) break;
+
+            b.minimax(0, Board.PLAYER_X);
+            System.out.println("Computer choose position: " + b.computerMove);
+
+            b.placeAMove(b.computerMove, Board.PLAYER_X);
+            b.displayBoard();
+        }
+
+        if(b.hasPlayerWon(Board.PLAYER_X))
+            System.out.println("You lost");
+
+        else if(b.hasPlayerWon(Board.PLAYER_O))
+            System.out.println("You win");
+        else
+            System.out.println("Draw");
+    }
+}
